9341628aae6d201948f33981c5d3d9f6eaf2f9fc
alan <alanzapadka@yahoo.com>
2024-12-12 20:39:43 +0000

    Form_commit

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,30 @@
+import {useForm} from "react-hook-form"
+import React from 'react';
+
+interface IFormInput{
+    Name: string,
+    Email: string,
+    Password: string
+}
+
+function Input() {
+    const {register} = useForm<IFormInput>()
+    
+    return (
+        <form>
+    <input {...register("Name", {minLength: {value: 3, message: "min length is 3"}, 
+    required: "This input is required"}, )} placeholder="Name" 
+    />
+    <input {...register("Email", {required: "This input is required"}, )} placeholder="Email" 
+    />
+     <input {...register("Email", {required: "This input is required", minLength: {value: 8, message: "min length is 8"}}, )} placeholder="Password" 
+    />
+ </form>
+   
+
+
+
+)
+}
+
+export default Input
